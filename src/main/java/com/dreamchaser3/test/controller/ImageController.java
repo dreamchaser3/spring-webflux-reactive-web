b7bf85496c7246d911ad9ce47138611a572c53b1
dreamchaser3 <dreamchaser3@naver.com>
2019-04-05 19:50:21 +0900

    Construct reactive controller and service

--- src/main/java/com/dreamchaser3/test/controller/ImageController.java
+++ src/main/java/com/dreamchaser3/test/controller/ImageController.java
@@ -1,31 +1,58 @@
 package com.dreamchaser3.test.controller;
 
-import com.dreamchaser3.test.model.Image;
+import com.dreamchaser3.test.service.ImageService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+
 @Slf4j
-@RestController
+@Controller
 public class ImageController {
-    @GetMapping("/images")
-    Flux<Image> images() {
-        return Flux.just(new Image("1", "test1.jpg"),
-                new Image("2", "test2.jpg"),
-                new Image("3", "test3.jpg"));
+    private static final String FILE_NAME = "{filename:.+}";
+    @Autowired
+    private ImageService imageService;
+
+    @GetMapping(value = "/images/" + FILE_NAME + "/raw", produces = MediaType.IMAGE_JPEG_VALUE)
+    @ResponseBody
+    public Mono<ResponseEntity<?>> oneRawImage(@PathVariable String fileName) {
+        return imageService.findOneImage(fileName)
+                .map(resource -> {
+                    try {
+                        return ResponseEntity.ok()
+                                .contentLength(resource.contentLength())
+                                .body(new InputStreamResource(resource.getInputStream()));
+                    } catch (IOException e) {
+                        return ResponseEntity.badRequest()
+                                .body("Couldn't find the file.");
+                    }
+                });
     }
 
     @PostMapping("/images")
-    Mono<Void> create(@RequestBody Flux<Image> images) {
-        return images
-                .map(image -> {
-                    log.info("We will save " + image);
-                    return image;
-                })
-                .then();
+    public Mono<String> createFile(@RequestPart(name = "file") Flux<FilePart> files) {
+        return imageService.createImage(files)
+                .then(Mono.just("redirect:/"));
+    }
+
+    @DeleteMapping("/images/" + FILE_NAME)
+    public Mono<String> deleteFile(@PathVariable String fileName) {
+        return imageService.deleteImage(fileName)
+                .then(Mono.just("redirect:/"));
+    }
+
+    @GetMapping("/")
+    public Mono<String> index(Model model) {
+        model.addAttribute("images", imageService.findAllImages());
+        return Mono.just("index");
     }
 }
